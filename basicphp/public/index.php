a1012ec354ca2bbcad5aeb90897372514ef63aee
phpcrazy <phpcrazy@gmail.com>
2018-11-03 17:42:00 +0630

    First Commit

---
+++ basicphp/public/index.php
@@ -0,0 +1,14 @@
+<?php 
+require "../app/data.php";
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $home; ?></title>
+</head>
+<body>
+	<h1><?php echo $title; ?></h1>
+	<p><?php echo $body; ?></p>
+</body>
+</html>
