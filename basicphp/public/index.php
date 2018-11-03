933033a73a56d899e86c4486c75cb21e91fde8be
phpcrazy <phpcrazy@gmail.com>
2018-11-03 18:10:20 +0630

    HEREDOC

--- basicphp/public/index.php
+++ basicphp/public/index.php
@@ -1,14 +1,22 @@
 <?php 
-require "../app/data.php";
- ?>
+if(isset($_GET['page'])) {
+	$page = htmlspecialchars($_GET['page']);
+	echo $page;
+} else {
+echo <<<EOD
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title><?php echo $home; ?></title>
-</head>
-<body>
-	<h1><?php echo $title; ?></h1>
-	<p><?php echo $body; ?></p>
-</body>
-</html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Home</title>
+ </head>
+ <body>
+ 	<h1>Hello World</h1>
+ </body>
+ </html>
+EOD;
+}
+
+ ?>
+
+ 
