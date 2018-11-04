b64ff1e2710ba15bd8e301d8b485557a44aeabb4
phpcrazy <phpcrazy@gmail.com>
2018-11-04 17:52:10 +0630

    Output Buffering

--- basicphp/wpa30/functions.php
+++ basicphp/wpa30/functions.php
@@ -3,10 +3,12 @@
 function _load_view($view, $data = null) {
 	$file = DD . "app/view/" . $view . ".php";
 	if(file_exists($file)) {
+		ob_start();
 		if($data != null) {
 			extract($data);	
 		}
 		require $file;
+		ob_end_flush();
 	} else {
 		trigger_error("View File ထည့္ရမယ္ဆိုတာ မသိဘူးလား ခ်ီးထုတ္", E_USER_ERROR);
 	}
