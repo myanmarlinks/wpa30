ebe73b145acd6527b020298702c3cb50fcac3d5b
phpcrazy <phpcrazy@gmail.com>
2018-11-10 18:12:39 +0630

    Table Data

--- basicphp/app/controllers/controllers.php
+++ basicphp/app/controllers/controllers.php
@@ -2,7 +2,24 @@
 
 function _HomeController() {
 	$data = [
-		'title'	=> 'Myanmar Links'
+		'title'	=> 'Myanmar Links',
+		'students'	=> [
+			[
+				'id'	=> 1,
+				'name'	=> 'Aung Aung',
+				'address'	=> 'Hledan'
+			],
+			[
+				'id'	=> 2,
+				'name'	=> 'Bung Aung',
+				'address'	=> 'Hledan'
+			],
+			[
+				'id'	=> 3,
+				'name'	=> 'Cung Aung',
+				'address'	=> 'Hledan'
+			]
+		]
 	];
 	_load_view("index", $data);
 }
--- basicphp/app/view/index.php
+++ basicphp/app/view/index.php
@@ -3,10 +3,35 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Home</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-	<h1><?php echo $title ?></h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id quod eaque itaque adipisci? Tenetur distinctio deleniti quos neque unde, porro voluptatibus inventore, numquam, cupiditate accusantium sed cumque! At, reiciendis vero!</p>
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-12">
+				<h1><?php echo $title ?></h1>
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Address</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach($students as $student) { ?>
+							<tr>
+								<td><?php echo $student['id']; ?></td>
+								<td><?php echo $student['name']; ?></td>
+								<td><?php echo $student['address']; ?></td>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	
 </body>
 </html>
