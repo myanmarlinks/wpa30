81204525bd57101db465c540274d8c7da79a76b9
phpcrazy <phpcrazy@gmail.com>
2018-11-10 17:48:33 +0630

    Pretty URL

--- basicphp/public/index.php
+++ basicphp/public/index.php
@@ -1,9 +1,18 @@
 <?php 
+
 define("DD", "../");
 require DD . "wpa30/functions.php";
 require DD . "app/controllers/controllers.php";
 
-$page = isset($_GET['page']) ? htmlspecialchars($_GET['page']) : "home";
+$request_uri = explode("/", $_SERVER['REQUEST_URI']);
+$script_name = explode("/", $_SERVER['SCRIPT_NAME']);
+$uri = array_values(array_diff($request_uri, $script_name));
+
+if(empty($uri)) {
+	$page = "home";
+} else {
+	$page = $uri[0];
+}
 
 $controller = "_" . $page . "controller";
 if(function_exists($controller)) 
